8003fb125c5c572c90962ee1ea847c546505f796
marekwietecki <112908504+marekwietecki@users.noreply.github.com>
2022-10-25 01:09:07 +0200

    Create 3liczbyOd1do1000.java

---
+++ 3liczbyOd1do1000.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner skaner=new Scanner(System.in);
+        int a=1;
+        while(a<1001){
+            System.out.println(a);
+            a++;
+        }
+
+    }
+}

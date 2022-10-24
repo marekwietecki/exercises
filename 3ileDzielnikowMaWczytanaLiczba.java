e9b1504b722d5fcc67414f55d7afc831e21ec5c3
marekwietecki <112908504+marekwietecki@users.noreply.github.com>
2022-10-25 01:27:04 +0200

    Create 3ileDzielnikowMaWczytanaLiczba.java

---
+++ 3ileDzielnikowMaWczytanaLiczba.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner skaner=new Scanner(System.in);
+        int a,b,c;
+        b=1;
+        c=0;
+        System.out.println("wprowadz liczbe calkowita: ");
+        a = skaner.nextInt();
+        while(a>b) {
+            if (a % b == 0) {
+                System.out.println("dzielnikiem liczby "+a+" jest liczba: "+b);
+                c++;
+            } b++;
+        } System.out.println("liczba dzielnikow liczby: "+a+" wynosi: " + c);
+    }
+}
